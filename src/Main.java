c675777d9dd0b34f805390def4407481079db8f1
gigachad098 <xanderlockard14@gmail.com>
2023-01-18 21:39:41 -0500

    Not Working

--- src/Main.java
+++ src/Main.java
@@ -24,17 +24,44 @@ public class Main {
             System.out.println("Error: No Input in file");
             return;
         } // Stop the program if there is no input in the file.
-        int currenttime;
+        int currenttime = -1;
+        String[] currentLine = new String[0];
+        int requestTime = 0;
         boolean firstLoop = true;
-        while (input.hasNextLine()   {
+        while (input.hasNextLine())   {
             if (firstLoop) {
-
+                currentLine = breakUpLine(input.nextLine());
+                currenttime = Integer.parseInt(currentLine[1]);
+                requestTime = Integer.parseInt(currentLine[1]);
             }
+            if (currenttime == requestTime) {
+                if (currentLine[0].equals("CustomerOrder")) {
+                    orderList.addLast(new CustomerOrder(Integer.parseInt(currentLine[1]), currentLine[2], Integer.parseInt(currentLine[3]), Integer.parseInt(currentLine[4])));
+                }// If a customer order has been requested create it and add it to the list.
+                else if (currentLine[0].equals("PrintAvailableWorkerList")) {
 
+                }
+                else if (currentLine[0].equals("PrintWorkerAssignmentList")) {
+                }
+                else if (currentLine[0].equals("PrintMaxFulfillmentTime")) {
+
+                }
+                else {
+                    System.out.println("Error");
+                    return;
+                }
+                currentLine = input.nextLine().split("\s");
+                requestTime = Integer.parseInt(currentLine[1]);
+            } // If the current time matches with the new request,
+            // complete the request and ping for a new one
+            if ((orderList.last().bundleAble) && (currenttime - orderList.last().getOrderTime() < 5)) {
+                // look for a bundle
+            }
+            currenttime = incrementTime(currenttime);
         }
         // Linked list contains the order of the available workers set not to change
     }
-    public int incrementTime(int currenttime) {
+    public static int incrementTime(int currenttime) {
         int hours = currenttime/100;
         int minutes = currenttime%100;
         minutes++;
@@ -43,5 +70,13 @@ public class Main {
             hours++;
         }
         return (hours*100)+minutes;
+    }
+    public static String[] breakUpLine(String inputLine) {
+        String[] brokenUp = inputLine.split("\s");
+        return brokenUp;
+    }
+    public String printAvailableWorkerList(SinglyLinkedList<WorkerAssignment> workerList) {
+        String output = "AvailableWorkerList " + workerList;
+        return output;
     }
 }
